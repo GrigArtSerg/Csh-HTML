2106ac9ac213e4203d3f91c6b3abbf3c60a76a6d
GrigArtSerg <56555610+GrigArtSerg@users.noreply.github.com>
2022-10-04 15:34:03 +0300

    Fixed

--- Form1.cs
+++ Form1.cs
@@ -24,39 +24,6 @@ namespace HTMLTest_3
             }
         }
 
-        private string PrintDom(HtmlElementCollection elemColl, System.Text.StringBuilder returnStr, Int32 depth)
-        {
-            System.Text.StringBuilder str = new System.Text.StringBuilder();
-
-            foreach (HtmlElement elem in elemColl)
-            {
-                string elemName;
-
-                elemName = elem.GetAttribute("ID");
-                if (elemName == null || elemName.Length == 0)
-                {
-                    elemName = elem.GetAttribute("name");
-                    if (elemName == null || elemName.Length == 0)
-                    {
-                        elemName = "<no name>";
-                    }
-                }
-
-                str.Append(' ', depth * 4);
-                str.Append(elemName + ": " + elem.TagName + "(Level " + depth + ")");
-                returnStr.AppendLine(str.ToString());
-
-                if (elem.CanHaveChildren)
-                {
-                    PrintDom(elem.Children, returnStr, depth + 1);
-                }
-
-                str.Remove(0, str.Length);
-            }
-
-            return (returnStr.ToString());
-        }
-
         private void WB_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             HtmlDocument Doc = WB.Document;
